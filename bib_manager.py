b0d7ca99fb63d7125e2fa93ce4adb7ace8914995
sunluelectric <sunlu.electric@gmail.com>
2021-06-28 18:16:19 +0800

    minor update

--- bib_manager.py
+++ bib_manager.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Class BibManager defines a ".bib" file management tool for LaTeX users. 
-Class BibManager can create/open a ".bib" file and do the followings:
+Class BibManager can create/open a ".bib" file, and do the followings:
 (a) create/edit tree-structure table of contents;
 (b) insert the table of contents into the beginning of the ".bib" file;
 (c) insert a new reference into the ".bib" file under a tag;
