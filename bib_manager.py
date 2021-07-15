e19004b53906dc2f2c39227f6d1a95c56f00b508
sunluelectric <sunlu.electric@gmail.com>
2021-07-15 12:29:14 +0800

    minor update

--- bib_manager.py
+++ bib_manager.py
@@ -48,9 +48,8 @@ class BibManager:
             print("There is no bib file found in the given path. " \
                   + "A new bib file is created. ")
             self.path_bib = path_bib
-            file_bib = open(self.path_bib, 'w')
-            file_bib.write('%% - Name of bib file: ' + self.path_bib.split('/')[-1])
-            file_bib.close()
+            with open(self.path_bib, 'w') as file_bib:
+                file_bib.write('%% - Name of bib file: ' + self.path_bib.split('/')[-1])
     def read_bib(self):
         """
         readbib reads references items from self.path_bib, and store them in
@@ -58,9 +57,8 @@ class BibManager:
         contents is also read and stored in a 2D list.
         """
         print("Reading the bib file...")
-        # self.obj_table_of_contents = ?
-        # self.lst_refs = ?
-        pass
+        self.__read_table_of_contents_from_bib()
+        self.__add_refs_from_bib()
     def show_table_of_contents(self):
         """
         show_talbe_of_contents shows the table of content in the console.
@@ -78,9 +76,9 @@ class BibManager:
         self.obj_table_of_contents.create_table_of_contents_from_console()
         print("The following table of contents is created.")
         self.show_table_of_contents()
-    def add_reference_from_console(self):
+    def add_refs_from_console(self):
         """
-        add_reference_from_console reads the reference information from the
+        add_refs_from_console reads the reference information from the
         console and adds it to the reference dictionary.
         """
         print("Please key in the reference below. Register ONE reference only. " + \
@@ -95,7 +93,67 @@ class BibManager:
                 break
             lst_console_inputs.append(str_console_input)
         lst_console_inputs = self.__chop_list(lst_console_inputs)
-        for iter_item in lst_console_inputs:
+        self.__add_refs(lst_console_inputs)
+    def update_bib(self, path_output_bib = 'default', str_author_name = AUTHOR_NAME):
+        """
+        update_bib updates the bib file, including:
+        - print the path to the updated bib file (by default the same path as
+        self.path_bib, unless otherwise specified);
+        - generate time author name stamps in the bib file;
+        - print table of content in the bib file;
+        - (optional, by default) sort the references;
+        - print references in the bib file.
+        """
+        if path_output_bib == 'default':
+            path_output_bib = self.path_bib
+        print("The updated bib file will be stored at " + path_output_bib)
+        if os.path.isfile(path_output_bib):
+            print("Warning: This path points to an existing file. \
+                  The file will be over written.")
+        if self.__ask_yes_no("Do you want to continue?"):
+            print("Redirecting path to " + path_output_bib + "")
+            self.path_bib = path_output_bib
+            print("Updating bib file...")
+            with open(self.path_bib, 'w') as file_bib:
+                # time
+                str_print = "%% - Latest updated time: " + \
+                    datetime.now().strftime("%B %d, %Y %H:%M:%S")
+                file_bib.write(str_print + "\n")
+                print(str_print)
+                # author
+                str_print = "%% - Updated by: " + AUTHOR_NAME
+                file_bib.write(str_print + "\n")
+                print(str_print)
+                # table of Contents
+                if self.obj_table_of_contents is None:
+                    str_print = "%% - Table of contents: None"
+                    file_bib.write(str_print + "\n")
+                    print(str_print)
+                else:
+                    str_list = "%% - Table of contents: "
+                    file_bib.write(str_list + "\n")
+                    print(str_list)
+                    lst_print = self.obj_table_of_contents.return_table_of_contents()
+                    for iter_item in lst_print:
+                        str_list = "%% - > " + iter_item
+                        file_bib.write(str_list + "\n")
+                        print(str_list)
+                    str_print = "%% - End of Table of Contents"
+                    file_bib.write(str_print + "\n")
+                    print(str_print)
+            # references
+        else:
+            print("Abort: The bib file is not updated.")
+    def __read_table_of_contents_from_bib(self):
+        pass
+    def __add_refs_from_bib(self):
+        with open(self.path_bib, 'r') as file_bib:
+            lst_file_inputs = file_bib.readlines()
+            lst_file_inputs = [str_fileinput.strip() 
+                               for str_fileinput in lst_file_inputs]
+        self.__add_refs(lst_file_inputs)
+    def __add_refs(self, lst_text):
+        for iter_item in lst_text:
             if len(iter_item) > 0:
                 # type and id
                 lst_keyinfo = re.findall('@.*{', iter_item)
@@ -156,65 +214,13 @@ class BibManager:
                 if len(lst_keyinfo) > 0:
                     lst_keyinfo = re.findall('{.*}', lst_keyinfo[0])
                     obj_reference.str_publisher = lst_keyinfo[0][1:-1]
-                # obj_reference.str_type = ?
-                # end
+                # end of reference / hex_catid
                 if iter_item[0] == '}':
                     lst_keyinfo = re.findall('% catid = \d{8}', iter_item)
                     if len(lst_keyinfo) > 0:
                         lst_keyinfo = re.findall('\d{8}', lst_keyinfo[0])
                         obj_reference.hex_catid = int(lst_keyinfo[0])
                     self.dict_refs[obj_reference.str_id] = obj_reference
-    def update_bib(self, path_output_bib = 'default', str_author_name = AUTHOR_NAME):
-        """
-        update_bib updates the bib file, including:
-        - print the path to the updated bib file (by default the same path as
-        self.path_bib, unless otherwise specified);
-        - generate time author name stamps in the bib file;
-        - print table of content in the bib file;
-        - (optional, by default) sort the references;
-        - print references in the bib file.
-        """
-        if path_output_bib == 'default':
-            path_output_bib = self.path_bib
-        print("The updated bib file will be stored at " + path_output_bib)
-        if os.path.isfile(path_output_bib):
-            print("Warning: This path points to an existing file. \
-                  The file will be over written.")
-        if self.__ask_yes_no("Do you want to continue?"):
-            print("Redirecting path to " + path_output_bib + "")
-            self.path_bib = path_output_bib
-            print("Updating bib file...")
-            file_bib = open(self.path_bib, 'w')
-            # time
-            str_print = "%% - Latest updated time: " + \
-                datetime.now().strftime("%B %d, %Y %H:%M:%S")
-            file_bib.write(str_print + "\n")
-            print(str_print)
-            # author
-            str_print = "%% - Updated by: " + AUTHOR_NAME
-            file_bib.write(str_print + "\n")
-            print(str_print)
-            # table of Contents
-            if self.obj_table_of_contents is None:
-                str_print = "%% - Table of contents: None"
-                file_bib.write(str_print + "\n")
-                print(str_print)
-            else:
-                str_list = "%% - Table of contents: "
-                file_bib.write(str_list + "\n")
-                print(str_list)
-                lst_print = self.obj_table_of_contents.return_table_of_contents()
-                for iter_item in lst_print:
-                    str_list = "%% - > " + iter_item
-                    file_bib.write(str_list + "\n")
-                    print(str_list)
-                str_print = "%% - End of Table of Contents"
-                file_bib.write(str_print + "\n")
-                print(str_print)
-            # references
-            file_bib.close()
-        else:
-            print("Abort: The bib file is not updated.")
     @staticmethod
     def __ask_yes_no(str_message):
         str_input = input(str_message + " (YES/NO): ")
